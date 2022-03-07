9eb1e9eb0220f8d38b99d28d1d220b00a0e6c6a4
nobu <mojo.nobu@gmail.com>
2022-03-07 20:27:16 +0900

    fmtチャンクの読み取り部作成

--- AdmBwfReader/WaveReader/WaveReader.cs
+++ AdmBwfReader/WaveReader/WaveReader.cs
@@ -5,12 +5,48 @@ using System.Text;
 
 namespace WaveReader
 {
+    public struct FmtChunk
+    {
+        /// <summary>
+        /// Chunk ID: "fmt "
+        /// </summary>
+        public string ckID;
+        /// <summary>
+        /// Chunk size: 16, 18 or 40 以降のfmtチャンクのサイズ？
+        /// </summary>
+        public int cksize;
+        /// <summary>
+        /// Format code
+        /// </summary>
+        public short wFormatTag;
+        /// <summary>
+        /// Number of interleaved channels
+        /// </summary>
+        public short nChannels;
+        /// <summary>
+        /// sample rate
+        /// </summary>
+        public int nSamplesPerSec;
+        /// <summary>
+        /// data rate
+        /// </summary>
+        public int nAvgBytesPerSec;
+        /// <summary>
+        /// Data block size (bytes)
+        /// </summary>
+        public short nBlockAlign;
+        /// <summary>
+        /// 	Bits per sample
+        /// </summary>
+        public short wBitsPerSample;
+
+    }
     class WaveReader
     {
         public string RiffFourCC;
         public int DataSize;
         public string WaveFourCC;
-        public string fmtChunk;
+        public FmtChunk fmtChunk;
 
         public void Parse(string filePath)
         {
@@ -22,6 +58,23 @@ namespace WaveReader
                     RiffFourCC = System.Text.Encoding.ASCII.GetString(binaryReader.ReadBytes(4));
                     DataSize = BitConverter.ToInt32(binaryReader.ReadBytes(4), 0);
                     WaveFourCC = System.Text.Encoding.ASCII.GetString(binaryReader.ReadBytes(4));
+
+                    while (true)
+                    {
+                        var chunk = System.Text.Encoding.ASCII.GetString(binaryReader.ReadBytes(4));
+
+                        if(chunk == "fmt ")
+                        {
+                            fmtChunk.ckID = chunk;
+                            fmtChunk.cksize = BitConverter.ToInt16(binaryReader.ReadBytes(2), 0);
+                            fmtChunk.wFormatTag = BitConverter.ToInt16(binaryReader.ReadBytes(2), 0);
+                            fmtChunk.nChannels = BitConverter.ToInt16(binaryReader.ReadBytes(2), 0);
+                            fmtChunk.nSamplesPerSec = BitConverter.ToInt32(binaryReader.ReadBytes(4), 0);
+                            fmtChunk.nAvgBytesPerSec = BitConverter.ToInt32(binaryReader.ReadBytes(4), 0);
+                            fmtChunk.nBlockAlign = BitConverter.ToInt16(binaryReader.ReadBytes(2), 0);
+                            fmtChunk.wBitsPerSample = BitConverter.ToInt16(binaryReader.ReadBytes(2), 0);
+                        }
+                    }
                 }
                 catch
                 {
